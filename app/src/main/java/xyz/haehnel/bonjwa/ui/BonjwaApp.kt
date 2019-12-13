6ec629084b35ef8ebf558088e895060f1a836c0d
Mark Hähnel <hello@markhaehnel.de>
2019-12-13 12:24:51 +0100

    :recycle: Extraced drawer to seperate file

--- app/src/main/java/xyz/haehnel/bonjwa/ui/BonjwaApp.kt
+++ app/src/main/java/xyz/haehnel/bonjwa/ui/BonjwaApp.kt
@@ -1,16 +1,18 @@
 package xyz.haehnel.bonjwa.ui
 
-import androidx.annotation.DrawableRes
 import androidx.compose.Composable
 import androidx.compose.state
 import androidx.compose.unaryPlus
 import androidx.ui.animation.Crossfade
-import androidx.ui.core.Modifier
 import androidx.ui.core.Text
 import androidx.ui.core.dp
-import androidx.ui.graphics.Color
-import androidx.ui.layout.*
-import androidx.ui.material.*
+import androidx.ui.layout.Column
+import androidx.ui.layout.Row
+import androidx.ui.layout.Spacing
+import androidx.ui.layout.WidthSpacer
+import androidx.ui.material.DrawerState
+import androidx.ui.material.MaterialTheme
+import androidx.ui.material.ModalDrawerLayout
 import androidx.ui.material.surface.Surface
 import androidx.ui.res.stringResource
 import xyz.haehnel.bonjwa.BuildConfig
@@ -26,7 +28,7 @@ fun BonjwaApp() {
             drawerState = drawerState,
             onStateChange = onDrawerStateChange,
             drawerContent = {
-                AppDrawer(
+                BonjwaAppDrawer(
                     currentScreen = BonjwaStatus.currentScreen,
                     closeDrawer = { onDrawerStateChange(DrawerState.Closed) }
                 )
@@ -49,132 +51,59 @@ private fun AppContent(openDrawer: () -> Unit) {
 }
 
 @Composable
-private fun AppDrawer(
+private fun BonjwaAppDrawer(
     currentScreen: Screen,
     closeDrawer: () -> Unit
 ) {
-    Column(modifier = Expanded, arrangement = Arrangement.Begin) {
-        HeightSpacer(24.dp)
-        Row(modifier = Spacing(16.dp)) {
-            VectorImage(id = R.drawable.ic_logo, tint = (+MaterialTheme.colors()).onBackground)
-            WidthSpacer(16.dp)
-            Column {
-                Text(
-                    "${+stringResource(R.string.app_name)} ${+stringResource(R.string.schedule)}",
-                    style = (+MaterialTheme.typography()).h6
-                )
-                Text(
-                    "Die inoffizielle Community App",
-                    style = (+MaterialTheme.typography()).subtitle1.copy(
-                        color = (+MaterialTheme.colors()).onPrimary.copy(alpha = 0.6f)
+    AppDrawer(
+        headerContent = {
+            Row(modifier = Spacing(16.dp)) {
+                VectorImage(id = R.drawable.ic_logo, tint = (+MaterialTheme.colors()).onBackground)
+                WidthSpacer(16.dp)
+                Column {
+                    Text(
+                        "${+stringResource(R.string.app_name)} ${+stringResource(R.string.schedule)}",
+                        style = (+MaterialTheme.typography()).h6
                     )
+                    Text(
+                        +stringResource(R.string.app_description),
+                        style = (+MaterialTheme.typography()).subtitle1.copy(
+                            color = (+MaterialTheme.colors()).onPrimary.copy(alpha = 0.6f)
+                        )
 
-                )
+                    )
+                }
             }
-        }
-        Divider(color = Color(0x14CCCCCC))
-        DrawerButton(
-            icon = R.drawable.ic_calendar,
-            label = +stringResource(R.string.schedule),
-            isSelected = currentScreen == Screen.Home
-        ) {
-            navigateTo(Screen.Home)
-            closeDrawer()
-        }
-        DrawerButton(
-            icon = R.drawable.ic_settings,
-            label = +stringResource(R.string.settings),
-            isSelected = currentScreen == Screen.Settings
-        ) {
-            navigateTo(Screen.Settings)
-            closeDrawer()
-        }
-        Divider(color = Color(0x14CCCCCC))
-        DrawerInfo(
+        },
+
+        bodyContent = {
+            DrawerButton(
+                icon = R.drawable.ic_calendar,
+                label = +stringResource(R.string.schedule),
+                isSelected = currentScreen == Screen.Home
+            ) {
+                navigateTo(Screen.Home)
+                closeDrawer()
+            }
+            DrawerButton(
+                icon = R.drawable.ic_settings,
+                label = +stringResource(R.string.settings),
+                isSelected = currentScreen == Screen.Settings
+            ) {
+                navigateTo(Screen.Settings)
+                closeDrawer()
+            }
+        },
+
+        footerContent = {
+            DrawerInfo(
                 text =
-                    """
+                """
                     ${BuildConfig.APPLICATION_ID}
                     ${BuildConfig.VERSION_NAME} (${BuildConfig.BUILD_TYPE})
                     """.trimIndent(),
                 icon = R.drawable.ic_info)
-    }
-}
-
-@Composable
-private fun DrawerButton(
-    modifier: Modifier = Modifier.None,
-    @DrawableRes icon: Int,
-    label: String,
-    isSelected: Boolean,
-    action: () -> Unit
-) {
-    val colors = +MaterialTheme.colors()
-    val textIconColor = if (isSelected) {
-        colors.onPrimary
-    } else {
-        colors.onPrimary.copy(alpha = 0.6f)
-    }
-    val backgroundColor = if (isSelected) {
-        colors.primary.copy(alpha = 0.5f)
-    } else {
-        colors.surface
-    }
-
-
-    Surface(
-        modifier = modifier ,
-        color = backgroundColor
-    ) {
-        Button(onClick = action, style = TextButtonStyle(), modifier = Spacing(left = 8.dp, top = 8.dp, right = 8.dp, bottom = 8.dp)) {
-            Row (ExpandedWidth, arrangement = Arrangement.Begin) {
-                VectorImage(
-                    modifier = Gravity.Center,
-                    id = icon,
-                    tint = textIconColor
-                )
-                WidthSpacer(16.dp)
-                Text(
-                    modifier = Gravity.Center,
-                    text = label,
-                    style = (+MaterialTheme.typography()).body1.copy(
-                        color = textIconColor
-                    )
-                )
-            }
         }
-    }
+    )
 }
 
-@Composable
-private fun DrawerInfo(
-    modifier: Modifier = Modifier.None,
-    @DrawableRes icon: Int? = null,
-    text: String,
-    action: (() -> Unit)? = null
-) {
-    val color = (+MaterialTheme.colors()).onPrimary.copy(0.6f)
-
-    Surface(
-        modifier = modifier
-    ) {
-        Button(onClick = action, style = TextButtonStyle(), modifier = Spacing(left = 8.dp, top = 4.dp, right = 8.dp, bottom = 4.dp)) {
-            Row (ExpandedWidth, arrangement = Arrangement.Begin) {
-                if (icon != null) {
-                    VectorImage(
-                        modifier = Gravity.Center,
-                        id = icon,
-                        tint = color
-                    )
-                    WidthSpacer(16.dp)
-                }
-                Text(
-                    modifier = Gravity.Center,
-                    text = text,
-                    style = (+MaterialTheme.typography()).subtitle2.copy(
-                        color = color
-                    )
-                )
-            }
-        }
-    }
-}
---
+++ app/src/main/java/xyz/haehnel/bonjwa/ui/Drawer.kt
@@ -0,0 +1,115 @@
+package xyz.haehnel.bonjwa.ui
+
+import androidx.annotation.DrawableRes
+import androidx.compose.Composable
+import androidx.compose.unaryPlus
+import androidx.ui.core.Modifier
+import androidx.ui.core.Text
+import androidx.ui.core.dp
+import androidx.ui.graphics.Color
+import androidx.ui.layout.*
+import androidx.ui.material.Button
+import androidx.ui.material.Divider
+import androidx.ui.material.MaterialTheme
+import androidx.ui.material.TextButtonStyle
+import androidx.ui.material.surface.Surface
+
+@Composable
+fun AppDrawer(
+    headerContent: @Composable() (() -> Unit)? = null,
+    bodyContent: @Composable() () -> Unit,
+    footerContent: @Composable() (() -> Unit)? = null
+) {
+    Column(modifier = Expanded, arrangement = Arrangement.Begin) {
+        HeightSpacer(24.dp)
+        if (headerContent != null) {
+            headerContent()
+            Divider(color = Color(0x14CCCCCC))
+        }
+
+        bodyContent()
+
+        if (footerContent != null) {
+            Divider(color = Color(0x14CCCCCC))
+            footerContent()
+        }
+    }
+}
+
+@Composable
+fun DrawerButton(
+    modifier: Modifier = Modifier.None,
+    @DrawableRes icon: Int,
+    label: String,
+    isSelected: Boolean,
+    action: () -> Unit
+) {
+    val colors = +MaterialTheme.colors()
+    val textIconColor = if (isSelected) {
+        colors.onPrimary
+    } else {
+        colors.onPrimary.copy(alpha = 0.6f)
+    }
+    val backgroundColor = if (isSelected) {
+        colors.primary.copy(alpha = 0.5f)
+    } else {
+        colors.surface
+    }
+
+    Surface(
+        modifier = modifier,
+        color = backgroundColor
+    ) {
+        Button(onClick = action, style = TextButtonStyle(), modifier = Spacing(left = 8.dp, top = 8.dp, right = 8.dp, bottom = 8.dp)) {
+            Row (ExpandedWidth, arrangement = Arrangement.Begin) {
+                VectorImage(
+                    modifier = Gravity.Center,
+                    id = icon,
+                    tint = textIconColor
+                )
+                WidthSpacer(16.dp)
+                Text(
+                    modifier = Gravity.Center,
+                    text = label,
+                    style = (+MaterialTheme.typography()).body1.copy(
+                        color = textIconColor
+                    )
+                )
+            }
+        }
+    }
+}
+
+@Composable
+fun DrawerInfo(
+    modifier: Modifier = Modifier.None,
+    @DrawableRes icon: Int? = null,
+    text: String,
+    action: (() -> Unit)? = null
+) {
+    val color = (+MaterialTheme.colors()).onPrimary.copy(0.6f)
+
+    Surface(
+        modifier = modifier
+    ) {
+        Button(onClick = action, style = TextButtonStyle(), modifier = Spacing(left = 8.dp, top = 4.dp, right = 8.dp, bottom = 4.dp)) {
+            Row (ExpandedWidth, arrangement = Arrangement.Begin) {
+                if (icon != null) {
+                    VectorImage(
+                        modifier = Gravity.Center,
+                        id = icon,
+                        tint = color
+                    )
+                    WidthSpacer(16.dp)
+                }
+                Text(
+                    modifier = Gravity.Center,
+                    text = text,
+                    style = (+MaterialTheme.typography()).subtitle2.copy(
+                        color = color
+                    )
+                )
+            }
+        }
+    }
+}
